cfa982d343750b7f4401ed31d6f4a11502730666
eev41 <eev41@txstate.edu>
2026-02-12 07:25:12 -0600

    Created skeleton of methods to be used and a global static constant for array size

--- Grocery.java
+++ Grocery.java
@@ -1,3 +1,29 @@
 public class Grocery {
-   
+   static final int SIZE = 10; // array size constant
+
+   /**
+	 * Javadoc here
+	 */
+   public static void printInventory(String[] names, double[] prices, 
+      int[] stocks)
+   {
+
+   }
+
+   /**
+	 * Javadoc here
+	 */
+   public static void printInventory(String[] names, int[] stocks, 
+      String target, int amount)
+   {
+
+   }
+
+   /**
+	 * Javadoc Here
+	 */
+   public static void main(String[] args)
+   {
+
+   }
 }
