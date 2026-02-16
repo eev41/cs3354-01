7f10ff8825957fa65052cfe6055a2097e3ba341a
eev41 <eev41@txstate.edu>
2026-02-16 16:21:06 -0600

    Added Javadoc skeleton for class and all methods and filled in documentation for class and main method

--- Grocery.java
+++ Grocery.java
@@ -1,28 +1,49 @@
 import java.util.Scanner;
 
+/**
+ * Defines a class for a command line grocery management system 
+ * which allows its user to (1) see inventory of items and (2) 
+ * restock a specific item by a specified quantity.
+ * 
+ * @author Ananya Jaiswal
+ * @author Abinash Ghimire
+ * @author Isla Shih
+ */
 public class Grocery {
    static final int SIZE = 10; // array size constant
 
    /**
-	 * Javadoc here
+	 * Javadoc here.
+    * 
+    * @param names description
+    * @param prices description
+    * @param stocks description
 	 */
    public static void printInventory(String[] names, double[] prices, 
       int[] stocks)
    {
-
+      System.out.println("Test");
    }
 
    /**
-	 * Javadoc here
+	 * Javadoc here.
+    * 
+    * @param names description
+    * @param stocks description
+    * @param target description
+    * @param amount description
 	 */
    public static void restockItem(String[] names, int[] stocks, 
       String target, int amount)
    {
-
+      System.out.println(target + " " + amount);
    }
 
    /**
-	 * Javadoc Here
+	 * Prints a user menu from which other methods can be invoked
+    * and, if necessary, provided with inputs to be passed as arguments.
+    * 
+    * @param args not used
 	 */
    public static void main(String[] args)
    {
@@ -45,6 +66,7 @@ public class Grocery {
          System.out.println("3. Exit Program");
          System.out.print("Enter your choice: ");
          userChoice = userInput.nextInt();
+         userInput.nextLine();
 
          if (userChoice == 1)
             printInventory(itemNames, itemPrices, itemStocks);
@@ -62,5 +84,7 @@ public class Grocery {
          else if (userChoice != 3)
             System.out.println("Choice is invalid. Please re-enter.");
       }
+
+      userInput.close();
    }
 }
